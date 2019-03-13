02a37f56aff8053784c1ec09ba4205f5f2ac5cd9
Aliaksandr_Kruk <krukaliaksandr@gmail.com>
2019-03-13 13:49:30 +0300

    Split core file in 2 as index and dataHandling.Added complex properties comparsion

--- src/dataHandling.js
+++ src/dataHandling.js
@@ -1,9 +1,7 @@
 const fs = require('fs');
-const uri = 'https://swapi.co/api/people';
-const uriForPageCount = `https://swapi.co/api/people`;
-const todo = require('yargs');
-const pathToFile = `request3`;
 const got = require('got');
+
+const uri = 'https://swapi.co/api/people';
 
 const createAndWriteFile = (path, contents) => {
   if (!fs.existsSync(`./Data/${path}.json`)) {
@@ -25,108 +23,6 @@ const getPeopleCount = async (uri) => {
   const result = await getPage(uri);
   return result.body.count++;
 };
-
-// eslint-disable-next-line no-unused-expressions
-todo.command('Request', 'https://swapi.co/api/people/ ', function (yargs) {
-  return yargs.options({
-    'films': {
-      alias: 'fl',
-      describe: `List of films,in which character is shown`,
-      conflicts: 'phone',
-      array: false
-    },
-    'species': {
-      alias: 'l',
-      describe: `List of species`,
-      conflicts: 'phone',
-      array: false
-    },
-    'vehicles': {
-      alias: 'l',
-      describe: `List of the vehicles, in controling of which the character was involved`,
-      conflicts: 'phone',
-      array: false
-    },
-    'starships': {
-      alias: 'l',
-      describe: `List of starships, in controling of which the character was involved`,
-      conflicts: 'phone',
-      array: false
-    },
-    'obese': {
-      alias: 'o',
-      describe: 'Defines if the character is obese,formula is weight(kilo)/height(meeter)^2>=25',
-      demandOption: false
-    },
-    'name': {
-      alias: 'n',
-      describe: 'Name of the character',
-      demandOption: false
-    },
-    'height': {
-      alias: 'h',
-      describe: 'Height of the character',
-      demandOption: false
-    },
-    'mass': {
-      alias: 'm',
-      describe: 'Mass of the character',
-      demandOption: false
-    },
-    'hair_color': {
-      alias: 'h',
-      describe: 'Hair color of the character',
-      demandOption: false
-    },
-    'eye_color': {
-      alias: 'e',
-      describe: 'Eye color of the character',
-      demandOption: false
-    },
-    'skin_color': {
-      alias: 's',
-      describe: 'Skin color of the character',
-      demandOption: false
-    },
-    'birth_year': {
-      alias: 'b',
-      describe: 'Birth of the character',
-      demandOption: false
-    },
-    'gender': {
-      alias: 'g',
-      describe: 'Gender of the character',
-      demandOption: false
-    },
-    'homeworld': {
-      alias: 'w',
-      describe: 'homeworld of the character',
-      demandOption: false
-    },
-    'created': {
-      alias: 'c',
-      describe: 'Data of the last file modification',
-      demandOption: false
-    },
-    'edited': {
-      alias: 'e',
-      describe: 'Data of the last file modification',
-      demandOption: false
-    },
-    'url': {
-      alias: 'u',
-      describe: 'Url of file',
-      demandOption: false
-    }
-  });
-},
-async function (argv) {
-  const peopleCount = await getPeopleCount(uriForPageCount);
-  const results = await processTheResults(peopleCount, argv);
-  createAndWriteFile(pathToFile, results);
-}).help()
-  .demandCommand(1, 'You need at least one command before moving on')
-  .argv;
 
 const filterRecords = (searchFiltersObject, jsonArray, filterParameter = 'includeMatches') => {
   console.log(jsonArray);
@@ -153,13 +49,16 @@ const filterRecords = (searchFiltersObject, jsonArray, filterParameter = 'includ
         case 'species':
         case 'vehicles':
         case 'starships':
-          if (!(`${property}` in charatcter)) {
+          console.log(`started`);
+          if (!(property in charatcter)) {
             charatcterComparsionResult = false;
           } else {
             let areAnyMatchesFound = false;
             charatcter[property].forEach((itemInCharacterProperty) => {
               searchFiltersObject[property].forEach((itemInSearchObjProperty) => {
-                if (itemInSearchObjProperty.includes(itemInCharacterProperty)) areAnyMatchesFound = true;
+                console.log(`${itemInSearchObjProperty}
+                  ${itemInCharacterProperty}`);
+                if (itemInCharacterProperty.includes(itemInSearchObjProperty)) areAnyMatchesFound = true;
               });
             });
             if (areAnyMatchesFound === false) charatcterComparsionResult = false;
@@ -188,9 +87,11 @@ const sortParams = (consoleArgs) => {
     obese: consoleArgs.obese,
     skin_color: consoleArgs.skin_color
   };
-  for (let complexProperty in complexProperties) {
+  for (let complexProperty of complexProperties) {
     if (complexProperty in consoleArgs) {
       searchParams[complexProperty] = consoleArgs[complexProperty].split(',');
+      console.log(consoleArgs[complexProperty]);
+      console.log(searchParams[complexProperty]);
     }
   }
   Object.keys(searchParams
@@ -199,6 +100,7 @@ const sortParams = (consoleArgs) => {
       delete searchParams[key];
     }
   });
+  console.log(`TESTOBJ${JSON.stringify(searchParams)}`);
   return searchParams;
 };
 
@@ -228,7 +130,7 @@ const addObesePropertyForObj = async (characterObj) => {
 };
 
 // optimize
-const processTheResults = async function (pageCount, consoleArgs) {
+const processTheResults = async (pageCount, consoleArgs) => {
   const arrayOfPromises = [];
   let pageNumberCounter = 1;
   while (pageNumberCounter <= pageCount) {
@@ -249,6 +151,12 @@ const processTheResults = async function (pageCount, consoleArgs) {
 
 module.exports = {
   processTheResults,
+  addObesePropertyForObj,
+  addObeseProperty,
+  fetchBodyOfRequest,
+  sortParams,
+  filterRecords,
+  getPeopleCount,
   getPage,
-  sortParams
+  createAndWriteFile
 };
---
+++ src/index.js
@@ -0,0 +1,108 @@
+
+const todo = require('yargs');
+const pathToFile = `../Data/request`;
+const { processTheResults, getPeopleCount, createAndWriteFile } = require('./dataHandling');
+
+const uriForPageCount = `https://swapi.co/api/people`;
+
+// eslint-disable-next-line no-unused-expressions
+todo.command('Request', 'https://swapi.co/api/people/ ', function (yargs) {
+  return yargs.options({
+    'films': {
+      alias: 'fl',
+      describe: `List of films,in which character is shown`,
+      conflicts: 'phone',
+      array: false
+    },
+    'species': {
+      alias: 'l',
+      describe: `List of species`,
+      conflicts: 'phone',
+      array: false
+    },
+    'vehicles': {
+      alias: 'l',
+      describe: `List of the vehicles, in controling of which the character was involved`,
+      conflicts: 'phone',
+      array: false
+    },
+    'starships': {
+      alias: 'l',
+      describe: `List of starships, in controling of which the character was involved`,
+      conflicts: 'phone',
+      array: false
+    },
+    'obese': {
+      alias: 'o',
+      describe: 'Defines if the character is obese,formula is weight(kilo)/height(meeter)^2>=25',
+      demandOption: false
+    },
+    'name': {
+      alias: 'n',
+      describe: 'Name of the character',
+      demandOption: false
+    },
+    'height': {
+      alias: 'h',
+      describe: 'Height of the character',
+      demandOption: false
+    },
+    'mass': {
+      alias: 'm',
+      describe: 'Mass of the character',
+      demandOption: false
+    },
+    'hair_color': {
+      alias: 'h',
+      describe: 'Hair color of the character',
+      demandOption: false
+    },
+    'eye_color': {
+      alias: 'e',
+      describe: 'Eye color of the character',
+      demandOption: false
+    },
+    'skin_color': {
+      alias: 's',
+      describe: 'Skin color of the character',
+      demandOption: false
+    },
+    'birth_year': {
+      alias: 'b',
+      describe: 'Birth of the character',
+      demandOption: false
+    },
+    'gender': {
+      alias: 'g',
+      describe: 'Gender of the character',
+      demandOption: false
+    },
+    'homeworld': {
+      alias: 'w',
+      describe: 'homeworld of the character',
+      demandOption: false
+    },
+    'created': {
+      alias: 'c',
+      describe: 'Data of the last file modification',
+      demandOption: false
+    },
+    'edited': {
+      alias: 'e',
+      describe: 'Data of the last file modification',
+      demandOption: false
+    },
+    'url': {
+      alias: 'u',
+      describe: 'Url of file',
+      demandOption: false
+    }
+  });
+},
+async function (argv) {
+  const peopleCount = await getPeopleCount(uriForPageCount);
+  const results = await processTheResults(peopleCount, argv);
+  createAndWriteFile(pathToFile, results);
+}).help()
+  .demandCommand(1, 'You need at least one command before moving on')
+  .argv;
